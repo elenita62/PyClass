a4541f0a26b911941398fe6c3affe64c854271cd
elenita62 <ingelenalopez@hotmail.com>
2019-04-09 15:34:50 -0400

    Adding new datasets

--- testing.py
+++ testing.py
@@ -1,7 +1,24 @@
 # install anaconda-client
 import pandas as pd
+import numpy as np
 import csv
-data = pd.read_csv(r"C:\Users\Elena López\Documents\GitHub\python-ml-course\datasets\titanic\titanic3.csv", sep=",")
+import os
+
+dataMainPath = "./datasource"
+fileName = "titanic3.csv"
+fullPath = dataMainPath + "/" + fileName
+##Otra forma de hacer lo del path es con la siguiente funcion
+
+fullPath2 = os.path.join(dataMainPath, fileName)
+
+data = pd.read_csv(fullPath2, sep=",",dtype={"ingresos":np.float64, "edad":np.int32},
+                   header=0, skip_blank_lines=True)
 #data.reader = csv.reader(data)
-print(data.head)
+#print(data)
     
+ ##Cargando otro archivo, ahora tipo txt
+fileNameTxt = "Customer Churn Model.txt"
+fullPathTxt = os.path.join(dataMainPath, fileNameTxt)
+data2 = pd.read_csv(fullPathTxt)
+print(data2.head)
+
